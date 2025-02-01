65e56621b3100548b4460eff1fe2df31c9c8f6a7
Jonathon Doetsch <jonathon.doetsch@gmail.com>
2025-02-01 14:25:27 -0800

    connected to mongodb cluster

--- DotNetWebApi/Models/FootballPlayerModel.cs
+++ DotNetWebApi/Models/FootballPlayerModel.cs
@@ -1,7 +1,13 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace dotnet_api_demo.Models
 {
     public class FootballPlayerModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UniformNumber { get; set; }
---
+++ DotNetWebApi/Repositories/IFootballPlayerRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dotnet_api_demo.Models;
+
+namespace dotnet_api_demo.Repositories
+{
+    public interface IFootballPlayerRepository
+    {
+        Task<IEnumerable<FootballPlayerModel>> GetAllAsync();
+        Task<FootballPlayerModel?> GetByIdAsync(string id);
+        Task CreateAsync(FootballPlayerModel player);
+        Task<bool> UpdateAsync(string id, FootballPlayerModel player);
+        Task<bool> DeleteAsync(string id);
+    }
+}
