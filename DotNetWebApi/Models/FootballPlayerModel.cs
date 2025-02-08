ec551776e48800df7bb34290b617a2891013c65c
Jonathon Doetsch <jonathon.doetsch@gmail.com>
2025-02-08 13:27:13 -0800

    using record type in Models layer

--- DotNetWebApi/Models/FootballPlayerModel.cs
+++ DotNetWebApi/Models/FootballPlayerModel.cs
@@ -1,17 +1,13 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace dotnet_api_demo.Models
-{
-    public class FootballPlayerModel
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string UniformNumber { get; set; }
-        public FootballTeamModel CurrentTeam { get; set; }
-        public List<FootballTeamModel> TeamHistory { get; set; }
-    }
-}
+namespace dotnet_api_demo.Models;
+
+public record FootballPlayerModel(
+    string FirstName,
+    string LastName,
+    string UniformNumber,
+    FootballTeamModel? CurrentTeam = null,
+    List<FootballTeamModel>? TeamHistory = null,
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string Id = ""
+);
--- DotNetWebApi/Models/FootballTeamModel.cs
+++ DotNetWebApi/Models/FootballTeamModel.cs
@@ -1,15 +1,11 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace dotnet_api_demo.Models 
-{
-    public class FootballTeamModel
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
-        public string TeamName { get; set; }
-        public string TeamCity { get; set; }
-        public List<FootballPlayerModel> CurrentRoster { get; set; }
-    }
-}
+namespace dotnet_api_demo.Models;
+
+public record FootballTeamModel(
+    string TeamName,
+    string TeamCity,
+    List<FootballPlayerModel>? CurrentRoster = null,
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string Id = ""
+);
