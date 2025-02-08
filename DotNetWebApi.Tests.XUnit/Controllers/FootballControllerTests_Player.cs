3ff2f6956fbec8d7156c33af87867df756de6a3a
Jonathon Doetsch <jonathon.doetsch@gmail.com>
2025-02-08 12:23:32 -0800

    test namespace updates

--- DotNetWebApi.Tests.XUnit/Controllers/FootballControllerTests_Player.cs
+++ DotNetWebApi.Tests.XUnit/Controllers/FootballControllerTests_Player.cs
@@ -8,103 +8,102 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace dotnet_api_demo.Tests.XUnit
+namespace dotnet_api_demo.Tests.XUnit;
+
+public class FootballControllerTests
 {
-    public class FootballControllerTests
+    private IFootballTeamService _footballTeamService;
+    private IFootballPlayerService _footballPlayerService;
+    private FootballController _controller;
+
+    public FootballControllerTests()
     {
-        private IFootballTeamService _footballTeamService;
-        private IFootballPlayerService _footballPlayerService;
-        private FootballController _controller;
+        var footballTeamServiceMock = new Mock<IFootballTeamService>();
+        var footballPlayerServiceMock = new Mock<IFootballPlayerService>();
 
-        public FootballControllerTests()
-        {
-            var footballTeamServiceMock = new Mock<IFootballTeamService>();
-            var footballPlayerServiceMock = new Mock<IFootballPlayerService>();
-
-            footballPlayerServiceMock.Setup(service => service.GetAllPlayersAsync())
-                .ReturnsAsync(new List<FootballPlayerModel>
+        footballPlayerServiceMock.Setup(service => service.GetAllPlayersAsync())
+            .ReturnsAsync(new List<FootballPlayerModel>
+            {
+                new FootballPlayerModel
                 {
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Nico", 
-                        LastName = "Collins", 
-                        UniformNumber = "4", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "CJ", 
-                        LastName = "Stroud", 
-                        UniformNumber = "7", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "JJ", 
-                        LastName = "McCarthy", 
-                        UniformNumber = "9", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Vikings", TeamCity = "Minnesota" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Vikings", TeamCity = "Minnesota" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Blake", 
-                        LastName = "Corum", 
-                        UniformNumber = "30", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Rams", TeamCity = "Los Angeles" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Rams", TeamCity = "Los Angeles" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Jaxon", 
-                        LastName = "Smith Njigba", 
-                        UniformNumber = "11", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Seahawks", TeamCity = "Seattle" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Seahawks", TeamCity = "Seattle" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Garrett", 
-                        LastName = "Wilson", 
-                        UniformNumber = "17", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Jets", TeamCity = "New York" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Jets", TeamCity = "New York" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Chris", 
-                        LastName = "Olave", 
-                        UniformNumber = "12", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Saints", TeamCity = "New Orleans" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Saints", TeamCity = "New Orleans" } }
-                    },
-                    new FootballPlayerModel
-                    {
-                        FirstName = "Marvin", 
-                        LastName = "Harrison Jr", 
-                        UniformNumber = "18", 
-                        CurrentTeam = new FootballTeamModel { TeamName = "Cardinals", TeamCity = "Arizona" }, 
-                        TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Cardinals", TeamCity = "Arizona" } }
-                    }
-                });
+                    FirstName = "Nico", 
+                    LastName = "Collins", 
+                    UniformNumber = "4", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "CJ", 
+                    LastName = "Stroud", 
+                    UniformNumber = "7", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Texans", TeamCity = "Houston" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "JJ", 
+                    LastName = "McCarthy", 
+                    UniformNumber = "9", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Vikings", TeamCity = "Minnesota" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Vikings", TeamCity = "Minnesota" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "Blake", 
+                    LastName = "Corum", 
+                    UniformNumber = "30", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Rams", TeamCity = "Los Angeles" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Rams", TeamCity = "Los Angeles" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "Jaxon", 
+                    LastName = "Smith Njigba", 
+                    UniformNumber = "11", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Seahawks", TeamCity = "Seattle" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Seahawks", TeamCity = "Seattle" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "Garrett", 
+                    LastName = "Wilson", 
+                    UniformNumber = "17", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Jets", TeamCity = "New York" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Jets", TeamCity = "New York" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "Chris", 
+                    LastName = "Olave", 
+                    UniformNumber = "12", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Saints", TeamCity = "New Orleans" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Saints", TeamCity = "New Orleans" } }
+                },
+                new FootballPlayerModel
+                {
+                    FirstName = "Marvin", 
+                    LastName = "Harrison Jr", 
+                    UniformNumber = "18", 
+                    CurrentTeam = new FootballTeamModel { TeamName = "Cardinals", TeamCity = "Arizona" }, 
+                    TeamHistory = new List<FootballTeamModel> { new FootballTeamModel { TeamName = "Cardinals", TeamCity = "Arizona" } }
+                }
+            });
 
-            _footballTeamService = footballTeamServiceMock.Object;
-            _footballPlayerService = footballPlayerServiceMock.Object;
-            _controller = new FootballController(_footballPlayerService, _footballTeamService);
-        }
+        _footballTeamService = footballTeamServiceMock.Object;
+        _footballPlayerService = footballPlayerServiceMock.Object;
+        _controller = new FootballController(_footballPlayerService, _footballTeamService);
+    }
 
-        [Fact]
-        public async Task GetAllPlayers_ReturnsOkResult_WithPlayers()
-        {
-            var result = await _controller.GetAllPlayers();
-            var okResult = result as OkObjectResult;
-            Assert.NotNull(okResult);
+    [Fact]
+    public async Task GetAllPlayers_ReturnsOkResult_WithPlayers()
+    {
+        var result = await _controller.GetAllPlayers();
+        var okResult = result as OkObjectResult;
+        Assert.NotNull(okResult);
 
-            var players = okResult.Value as List<FootballPlayerModel>;
-            Assert.NotNull(players);
-            Assert.Equal(8, players.Count);
-        }
+        var players = okResult.Value as List<FootballPlayerModel>;
+        Assert.NotNull(players);
+        Assert.Equal(8, players.Count);
     }
 }
--- DotNetWebApi.Tests/Controllers/FootballControllerTests_Team.cs
+++ DotNetWebApi.Tests/Controllers/FootballControllerTests_Team.cs
@@ -8,73 +8,72 @@ using Shouldly;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace dotnet_api_demo.Tests
+namespace dotnet_api_demo.Tests;
+
+[TestFixture]
+public class FootballControllerTests
 {
-    [TestFixture]
-    public class FootballControllerTests
+    private IFootballTeamService _footballTeamService;
+    private IFootballPlayerService _footballPlayerService;
+    private FootballController _controller;
+
+    [SetUp]
+    public void SetUp()
     {
-        private IFootballTeamService _footballTeamService;
-        private IFootballPlayerService _footballPlayerService;
-        private FootballController _controller;
+        var footballTeamServiceMock = new Mock<IFootballTeamService>();
+        var footballPlayerServiceMock = new Mock<IFootballPlayerService>();
 
-        [SetUp]
-        public void SetUp()
-        {
-            var footballTeamServiceMock = new Mock<IFootballTeamService>();
-            var footballPlayerServiceMock = new Mock<IFootballPlayerService>();
+        footballTeamServiceMock.Setup(service => service.GetAllTeamsAsync())
+            .ReturnsAsync(new List<FootballTeamModel>
+            {
+                new FootballTeamModel { TeamCity = "Arizona", TeamName = "Cardinals", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Atlanta", TeamName = "Falcons", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Baltimore", TeamName = "Ravens", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Buffalo", TeamName = "Bills", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Carolina", TeamName = "Panthers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Chicago", TeamName = "Bears", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Cincinnati", TeamName = "Bengals", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Cleveland", TeamName = "Browns", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Dallas", TeamName = "Cowboys", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Denver", TeamName = "Broncos", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Detroit", TeamName = "Lions", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Green Bay", TeamName = "Packers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Houston", TeamName = "Texans", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Indianapolis", TeamName = "Colts", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Jacksonville", TeamName = "Jaguars", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Kansas City", TeamName = "Chiefs", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Las Vegas", TeamName = "Raiders", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Los Angeles", TeamName = "Chargers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Los Angeles", TeamName = "Rams", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Miami", TeamName = "Dolphins", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Minnesota", TeamName = "Vikings", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "New England", TeamName = "Patriots", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "New Orleans", TeamName = "Saints", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "New York", TeamName = "Giants", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "New York", TeamName = "Jets", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Philadelphia", TeamName = "Eagles", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Pittsburgh", TeamName = "Steelers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "San Francisco", TeamName = "49ers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Seattle", TeamName = "Seahawks", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Tampa Bay", TeamName = "Buccaneers", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Tennessee", TeamName = "Titans", CurrentRoster = new List<FootballPlayerModel>() },
+                new FootballTeamModel { TeamCity = "Washington", TeamName = "Commanders", CurrentRoster = new List<FootballPlayerModel>() }
+            });
 
-            footballTeamServiceMock.Setup(service => service.GetAllTeamsAsync())
-                .ReturnsAsync(new List<FootballTeamModel>
-                {
-                    new FootballTeamModel { TeamCity = "Arizona", TeamName = "Cardinals", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Atlanta", TeamName = "Falcons", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Baltimore", TeamName = "Ravens", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Buffalo", TeamName = "Bills", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Carolina", TeamName = "Panthers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Chicago", TeamName = "Bears", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Cincinnati", TeamName = "Bengals", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Cleveland", TeamName = "Browns", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Dallas", TeamName = "Cowboys", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Denver", TeamName = "Broncos", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Detroit", TeamName = "Lions", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Green Bay", TeamName = "Packers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Houston", TeamName = "Texans", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Indianapolis", TeamName = "Colts", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Jacksonville", TeamName = "Jaguars", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Kansas City", TeamName = "Chiefs", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Las Vegas", TeamName = "Raiders", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Los Angeles", TeamName = "Chargers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Los Angeles", TeamName = "Rams", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Miami", TeamName = "Dolphins", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Minnesota", TeamName = "Vikings", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "New England", TeamName = "Patriots", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "New Orleans", TeamName = "Saints", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "New York", TeamName = "Giants", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "New York", TeamName = "Jets", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Philadelphia", TeamName = "Eagles", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Pittsburgh", TeamName = "Steelers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "San Francisco", TeamName = "49ers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Seattle", TeamName = "Seahawks", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Tampa Bay", TeamName = "Buccaneers", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Tennessee", TeamName = "Titans", CurrentRoster = new List<FootballPlayerModel>() },
-                    new FootballTeamModel { TeamCity = "Washington", TeamName = "Commanders", CurrentRoster = new List<FootballPlayerModel>() }
-                });
+        _footballTeamService = footballTeamServiceMock.Object;
+        _footballPlayerService = footballPlayerServiceMock.Object;
+        _controller = new FootballController(_footballPlayerService, _footballTeamService);
+    }
 
-            _footballTeamService = footballTeamServiceMock.Object;
-            _footballPlayerService = footballPlayerServiceMock.Object;
-            _controller = new FootballController(_footballPlayerService, _footballTeamService);
-        }
-
-        [Test]
-        public async Task GetAllTeams_ReturnsOkResult_WithTeams()
-        {
-            var result = await _controller.GetAllTeams();
-            var okResult = result as OkObjectResult;
-            okResult.ShouldNotBeNull();
-            
-            var teams = okResult.Value as List<FootballTeamModel>;
-            teams.ShouldNotBeNull();
-            teams.Count.ShouldBe(32);
-        }
+    [Test]
+    public async Task GetAllTeams_ReturnsOkResult_WithTeams()
+    {
+        var result = await _controller.GetAllTeams();
+        var okResult = result as OkObjectResult;
+        okResult.ShouldNotBeNull();
+        
+        var teams = okResult.Value as List<FootballTeamModel>;
+        teams.ShouldNotBeNull();
+        teams.Count.ShouldBe(32);
     }
 }
